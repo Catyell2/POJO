18be05b3b4a32c6cae137c75e95ccde9c52c173a
James Jessup <j.jessup16@gmail.com>
2017-04-24 22:53:35 -0500

    Adds Fillable interface and fill() to Ink. #8

---
+++ edu/jalc/pen/ink/Fillable.java
@@ -0,0 +1,5 @@
+package edu.jalc.pen.ink;
+
+public interface Fillable{
+  public void fill();
+}
--- edu/jalc/pen/ink/Ink.java
+++ edu/jalc/pen/ink/Ink.java
@@ -1,8 +1,12 @@
 package edu.jalc.pen.ink;
 
-  public class Ink{
+  public class Ink implements Fillable{
     public void getInk(){
+      this.fill();
       System.out.println("I have Ink");
+    }
+    public void fill(){
+      System.out.println("Fills up the Ink");
     }
 
 
